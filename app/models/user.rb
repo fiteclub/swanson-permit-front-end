a538f818ce6bc7f3fd7acce453360b767960ff26
fiteclub <mattf255@yahoo.com>
2021-05-16 01:15:35 -0400

    Change user class to activeresource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
-class User < ApplicationRecord
+class User < ActiveResource::Base
+  self.site = "http://localhost:3000/api/v1"
+  self.element_name = "user"
+  self.include_format_in_path = false
 end
